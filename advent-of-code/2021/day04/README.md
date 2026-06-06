87fd754c2307dd6d4fb37583fbfb9ad42502215b
Matheus Araujo <matheus.saraujo@gmail.com>
2026-06-06 14:22:15 +0000

    Advent of Code 2021 - Day 04: Giant Squid

---
+++ advent-of-code/2021/day04/README.md
@@ -0,0 +1,5 @@
+# Advent of Code - 2021 Day 04
+
+Giant Squid
+
+https://adventofcode.com/2021/day/4
---
+++ advent-of-code/2021/day04/helpers.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <string>
+#include <vector>
+using namespace std;
+
+int playBingo(const vector<string> &puzzleInput, bool findLast);
